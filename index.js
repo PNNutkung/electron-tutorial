eb71b68b858b4d909cd6cc24cb8d0687a682d028
PNNutkung <pipatpol.t@ku.th>
2018-01-02 14:33:32 +0700

    :tada:

---
+++ index.js
@@ -0,0 +1,83 @@
+const {
+  app,
+  BrowserWindow,
+  Menu,
+  ipcMain
+} = require('electron')
+
+let mainWindow
+let addWindow
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({})
+  mainWindow.loadURL(`file://${__dirname}/main.html`)
+  mainWindow.on('closed', () => app.quit())
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate)
+  Menu.setApplicationMenu(mainMenu)
+})
+
+const createAddWindow = () => {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add New Todo'
+  })
+  addWindow.loadURL(`file://${__dirname}/add.html`)
+  addWindow.on('closed', () => addWindow = null)
+}
+
+ipcMain.on('todo:add', (event, todo) => {
+  mainWindow.webContents.send('todo:add', todo)
+  addWindow.close()
+})
+
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'New Todo',
+        accelerator: process.platform === 'darwin' ? 'Command+N' : 'Ctrl+N',
+        click() {
+          createAddWindow()
+        }
+      },
+      {
+        label: 'Clear todos',
+        click() {
+          mainWindow.webContents.send('todo:clearAll')
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click() {
+          app.quit()
+        }
+      }
+    ]
+  }
+]
+
+if (process.platform === 'darwin') {
+  menuTemplate.unshift({})
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  menuTemplate.push({
+    label: 'View',
+    submenu: [
+      {
+        role: 'reload'
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools()
+        }
+      }
+    ]
+  })
+}
